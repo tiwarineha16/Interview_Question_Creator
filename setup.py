26caea4a34dfce7280b1fbc642b2bfccaafbed6e
Neha Tiwari <nehatiwaribt@gmail.com>
2024-06-02 20:32:35 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+# to create a local package we need steup.py
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GEN AI Project',
+    version= '0.0.0',
+    author= 'Neha',
+    author_email= 'nehatiwaribt@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "InterviewQuestionCreatorProject"
+#project_name = "InterviewQuestionCreatorProject"
 
 
 list_of_files = [
